d59557a8bf6e7a52bc5379697c3119b457327922
Chanchal Kumar Chiranjivi <165770818+ChiranjiviChanchal@users.noreply.github.com>
2024-08-30 00:43:15 +0530

    Swap string with user input

---
+++ swap string.R
@@ -0,0 +1,12 @@
+name1<-readline("Sunny, Enter your wife name: ")
+ name2<-readline("Enter another wife name :  ")
+ temp1<-readline("Enter third person name :  ")
+ print("---------Before swapping-------")
+ print(paste("Before swapping sunny's wife name is ",name1))
+ print(paste("Before , swpping another wife name is ",name2))
+ prashant=name1
+ name1=name2
+ name2=prashant
+ print("------Aftter swpping--------")
+ print(paste("After swapping sunny's wife name is ",name1))
+ print(paste("After swapping sunny's wife name is ",name2))
