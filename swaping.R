911469486da61a7533cae43be68ae69e79ee3a1a
Chanchal Kumar Chiranjivi <165770818+ChiranjiviChanchal@users.noreply.github.com>
2024-08-30 00:39:20 +0530

    Swap number using third variable

---
+++ swaping.R
@@ -0,0 +1,11 @@
+ a<-readline("Enter any 1st number: ")
+ b<-readline("Enter another number :  ")
+ print("---------Before swapping-------")
+ print(a)
+ print(b)
+ temp=a
+ a=b
+ b=temp
+ print("------Aftter swpping--------")
+ print(a)
+ print(b)
